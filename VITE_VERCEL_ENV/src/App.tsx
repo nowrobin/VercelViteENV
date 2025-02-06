50d940f2306c5d440ecb5e19703fed5a6d686091
robin <nowrobin3@gmail.com>
2025-02-06 15:19:46 +0900

    VITEVERCEL

---
+++ VITE_VERCEL_ENV/src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  const viteKey = import.meta.env.VITE_KEY;
+  const notViteKey = import.meta.env.NOT_VITE_KEY;
+  const vercelKey = import.meta.env.VITE_VERCEL_KEY;
+  const notVercelKey = import.meta.env.NOT_VITE_VERCEL_KEY;
+  return (
+    <>
+      <div>{viteKey}</div>
+      <div>{notViteKey}</div>
+      <div>{vercelKey}</div>
+      <div>{notVercelKey}</div>
+    </>
+  )
+}
+
+export default App
---
+++ VITE_VERCEL_ENV/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    "process.env": process.env,
+    NOT_VITE_VERCEL_KEY: process.env.NOT_VITE_VERCEL_KEY,
+    VITE_VERCEL_KEY: process.env.VITE_VERCEL_KEY,
+  },
+});
