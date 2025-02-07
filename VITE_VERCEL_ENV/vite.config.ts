f2c0aabcb2dc5c8adaa17b25e2053756c60093aa
nowrobin <nowrobin3@gmail.com>
2025-02-07 16:46:57 +0900

    Update vite.config.ts

--- VITE_VERCEL_ENV/vite.config.ts
+++ VITE_VERCEL_ENV/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   define: {
     "process.env": process.env,
-    NOT_VITE_VERCEL_KEY: process.env.NOT_VITE_VERCEL_KEY,
+    VITE_NOT_VERCEL_KEY: process.env.NOT_VITE_VERCEL_KEY,
     VITE_VERCEL_KEY: process.env.VITE_VERCEL_KEY,
   },
 });
