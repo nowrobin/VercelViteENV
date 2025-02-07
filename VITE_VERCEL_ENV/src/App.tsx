8d51fe6bb6389e79cc5bd3b40c02bcbe3f069409
robin <nowrobin3@gmail.com>
2025-02-07 17:15:28 +0900

    a

--- VITE_VERCEL_ENV/src/App.tsx
+++ VITE_VERCEL_ENV/src/App.tsx
@@ -2,16 +2,25 @@
 import './App.css'
 
 function App() {
+
+  //VITE 추가/삭제  VERCEL 추가 삭제 
   const viteKey = import.meta.env.VITE_KEY;
   const notViteKey = import.meta.env.NOT_VITE_KEY;
   const vercelKey = import.meta.env.VITE_VERCEL_KEY;
   const notVercelKey = import.meta.env.NOT_VITE_VERCEL_KEY;
+  const viteVercel = import.meta.env.VITE_VERCEL_URL;
+  const vite = import.meta.env.VITE_URL;
+  const vercel = import.meta.env.VERCEL_URL;
   return (
     <>
+      <div></div>
       <div>{viteKey}</div>
       <div>{notViteKey}</div>
       <div>{vercelKey}</div>
       <div>{notVercelKey}</div>
+      <div>{viteVercel}</div>
+      <div>{vite}</div>
+      <div>{vercel}</div>
     </>
   )
 }
